dd1693a3c9d16f0cd5b5c5d5ddcbcb72b73ff72f
bjorn3 <bjorn3@users.noreply.github.com>
2022-02-07 18:39:26 +0100

    Remove the EnumVariantMeta trait
    Instead add an inherent implementation. This allows removing the
    bevy_derive dependency from bevy_utils.

--- crates/bevy_derive/src/lib.rs
+++ crates/bevy_derive/src/lib.rs
@@ -3,13 +3,12 @@ extern crate proc_macro;
 mod app_plugin;
 mod bevy_main;
 mod enum_variant_meta;
-mod modules;
 
 use bevy_macro_utils::{derive_label, BevyManifest};
 use proc_macro::TokenStream;
 use quote::format_ident;
 
-/// Generates a dynamic plugin entry point function for the given `Plugin` type.  
+/// Generates a dynamic plugin entry point function for the given `Plugin` type.
 #[proc_macro_derive(DynamicPlugin)]
 pub fn derive_dynamic_plugin(input: TokenStream) -> TokenStream {
     app_plugin::derive_dynamic_plugin(input)
--- crates/bevy_utils/src/lib.rs
+++ crates/bevy_utils/src/lib.rs
@@ -1,8 +1,6 @@
-mod enum_variant_meta;
 pub mod label;
 
 pub use ahash::AHasher;
-pub use enum_variant_meta::*;
 pub use instant::{Duration, Instant};
 pub use tracing;
 pub use uuid::Uuid;
