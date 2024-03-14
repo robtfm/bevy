5de966a959969eb69bf9c1f077bb2a4ac1bf5ec8
robtfm <50659922+robtfm@users.noreply.github.com>
2024-03-14 16:20:33 +0000

    soft error on missing serialize

--- crates/bevy_reflect/src/serde/ser.rs
+++ crates/bevy_reflect/src/serde/ser.rs
@@ -157,8 +157,19 @@ impl<'a> Serialize for TypedReflectSerializer<'a> {
                 registry: self.registry,
             }
             .serialize(serializer),
-            ReflectRef::Value(_) => Err(serializable.err().unwrap()),
+            ReflectRef::Value(_) => ErrSerializer(serializable.err().unwrap().to_string()).serialize(serializer),
         }
+    }
+}
+
+pub struct ErrSerializer(String);
+
+impl Serialize for ErrSerializer {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.0)
     }
 }
 
