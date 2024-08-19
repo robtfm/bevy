ca48b8daefcc59ff035746ee41a32e66a0f29ad5
robtfm <50659922+robtfm@users.noreply.github.com>
2024-08-20 00:08:51 +0100

    fix merge

--- crates/bevy_text/src/font_atlas.rs
+++ crates/bevy_text/src/font_atlas.rs
@@ -91,11 +91,10 @@ impl FontAtlas {
         offset: IVec2,
     ) -> Result<(), TextError> {
         let atlas_layout = atlas_layouts.get_mut(&self.texture_atlas).unwrap();
-        let atlas_texture = textures.get_mut(&self.texture).unwrap();
 
         if let Some(glyph_index) =
             self.dynamic_texture_atlas_builder
-                .add_texture(atlas_layout, texture, atlas_texture)
+                .add_texture(atlas_layout, textures, texture, &self.texture)
         {
             self.glyph_to_atlas_index.insert(
                 cache_key,
